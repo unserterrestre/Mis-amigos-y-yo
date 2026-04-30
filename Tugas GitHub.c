bbb80305870d9e2af4ce6512df92b4347e737af5
Vixhaf <vinthuxaftrievenhym@gmail.comm>
2026-04-30 15:05:08 +0700

    Ori langsung dari Vikko

--- Tugas GitHub.c
+++ Tugas GitHub.c
@@ -24,6 +24,28 @@ int main(){
       float hasil = bilangan_pertama - bilangan_kedua;
       printf("Hasil perhitungan ialah %.2f.", hasil);
     }
+    
+    else if(operasi == 3){
+      printf("\nProgram melaksanakan program perkalian.\nBerapakah bilangan pertama? Jawaban: ");
+      scanf("%f", &bilangan_pertama);
+      printf("Berapakah bilangan kedua? Jawaban: ");
+      scanf("%f", &bilangan_kedua);
+      float hasil = bilangan_pertama * bilangan_kedua;
+      printf("Hasil perhitungan ialah %.2f.", hasil);
+    }
+
+    else if(operasi == 4){
+      printf("\nProgram melaksanakan program pembagian.\nBerapakah bilangan pertama? Jawaban: ");
+      scanf("%f", &bilangan_pertama);
+      printf("Berapakah bilangan kedua? Jawaban: ");
+      scanf("%f", &bilangan_kedua);
+      float hasil = bilangan_pertama / bilangan_kedua;
+      printf("Hasil perhitungan ialah %.2f.", hasil);
+    }
+
+    else{
+      printf("Pilihan operasi tidak valid. Masukkan salah satu angka dari 1 sampai 4\n");
+    }
 
 	
 	printf("\n\nTerima kasih telah mempergunakan program kami.");
