93d01c0644cf69523928164f5ca9d9926a064a9b
No hay nada que entienda <216477226+unserterrestre@users.noreply.github.com>
2026-04-30 14:17:19 +0700

    Add subtraction operation prompts to Tugas GitHub.c

--- Tugas GitHub.c
+++ Tugas GitHub.c
@@ -7,5 +7,8 @@ if(operasi == 1){
   printf("Hasil perhitungan ialah %.2f.", hasil);
 }
 else if(operasi == 2){
-  
+  printf("\nProgram melaksanakan program pengurangan.\nBerapakah bilangan pertama? Jawaban: ");
+  scanf("%f", bilangan_pertama);
+  printf("Berapakah bilangan kedua? Jawaban: ");
+  scanf("%f", bilangan_kedua);
 }
