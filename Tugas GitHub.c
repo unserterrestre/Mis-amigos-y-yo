f7ece07e8dd28f204375beba882004e2665a4151
No hay nada que entienda <216477226+unserterrestre@users.noreply.github.com>
2026-04-30 14:18:35 +0700

    Fix scanf to use address operator for inputs

--- Tugas GitHub.c
+++ Tugas GitHub.c
@@ -1,16 +1,16 @@
 if(operasi == 1){
   printf("\nProgram melaksanakan program pertambahan.\nBerapakah bilangan pertama? Jawaban: ");
-  scanf("%f", bilangan_pertama);
+  scanf("%f", &bilangan_pertama);
   printf("Berapakah bilangan kedua? Jawaban: ");
-  scanf("%f", bilangan_kedua);
+  scanf("%f", &bilangan_kedua);
   float hasil = bilangan_pertama + bilangan_kedua;
   printf("Hasil perhitungan ialah %.2f.", hasil);
 }
 else if(operasi == 2){
   printf("\nProgram melaksanakan program pengurangan.\nBerapakah bilangan pertama? Jawaban: ");
-  scanf("%f", bilangan_pertama);
+  scanf("%f", &bilangan_pertama);
   printf("Berapakah bilangan kedua? Jawaban: ");
-  scanf("%f", bilangan_kedua);
+  scanf("%f", &bilangan_kedua);
   float hasil = bilangan_pertama - bilangan_kedua;
   printf("Hasil perhitungan ialah %.2f.", hasil);
 }
