2a1a72b496551ad85be0faaab05b8a2e4d355f1c
No hay nada que entienda <216477226+unserterrestre@users.noreply.github.com>
2026-04-30 14:14:47 +0700

    Add addition operation handling in Tugas GitHub.c

--- Tugas GitHub.c
+++ Tugas GitHub.c
@@ -1,1 +1,5 @@
-
+if(operasi == 1){
+  printf("\nProgram melaksanakan program pertambahan.\nBerapakah bilangan pertama? Jawaban: ");
+  scanf("%f", bilangan_pertama);
+  printf("Berapakah bilangan kedua? Jawaban: ");
+  scanf("%f", bilangan_kedua);
