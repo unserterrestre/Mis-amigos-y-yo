317f06269561a3e1b347305274ff88c439f235bd
harlandyoga-dev <harlandyoga@gmail.com>
2026-04-30 14:37:55 +0700

    Refactor simple calculator program structure
    Harland + NIko

--- Tugas GitHub.c
+++ Tugas GitHub.c
@@ -1,16 +1,31 @@
-if(operasi == 1){
-  printf("\nProgram melaksanakan program pertambahan.\nBerapakah bilangan pertama? Jawaban: ");
-  scanf("%f", &bilangan_pertama);
-  printf("Berapakah bilangan kedua? Jawaban: ");
-  scanf("%f", &bilangan_kedua);
-  float hasil = bilangan_pertama + bilangan_kedua;
-  printf("Hasil perhitungan ialah %.2f.", hasil);
-}
-else if(operasi == 2){
-  printf("\nProgram melaksanakan program pengurangan.\nBerapakah bilangan pertama? Jawaban: ");
-  scanf("%f", &bilangan_pertama);
-  printf("Berapakah bilangan kedua? Jawaban: ");
-  scanf("%f", &bilangan_kedua);
-  float hasil = bilangan_pertama - bilangan_kedua;
-  printf("Hasil perhitungan ialah %.2f.", hasil);
+#include <stdio.h>
+
+int main(){
+	int operasi;
+	float bilangan_pertama, bilangan_kedua;
+	
+	printf("Selamat datang di program matematika sederhana!\nProgram inim menyediakan kalkulator bagi operasi\n1. Pertambahan"
+	"\n2. Pengurangan\n3. Perkalian\n4. Pembagian\nPilihlah salah satu operasi: ");
+	scanf("%d", &operasi);
+
+	  if(operasi == 1){
+      printf("\nProgram melaksanakan program pertambahan.\nBerapakah bilangan pertama? Jawaban: ");
+      scanf("%f", &bilangan_pertama);
+      printf("Berapakah bilangan kedua? Jawaban: ");
+      scanf("%f", &bilangan_kedua);
+      float hasil = bilangan_pertama + bilangan_kedua;
+      printf("Hasil perhitungan ialah %.2f.", hasil);
+    }
+    else if(operasi == 2){
+      printf("\nProgram melaksanakan program pengurangan.\nBerapakah bilangan pertama? Jawaban: ");
+      scanf("%f", &bilangan_pertama);
+      printf("Berapakah bilangan kedua? Jawaban: ");
+      scanf("%f", &bilangan_kedua);
+      float hasil = bilangan_pertama - bilangan_kedua;
+      printf("Hasil perhitungan ialah %.2f.", hasil);
+    }
+
+	
+	printf("\n\nTerima kasih telah mempergunakan program kami.");
+	return 0;
 }
